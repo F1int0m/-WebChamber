b47e25edbfa2178dd9df0355ba15455544b56d98
Nikita Buguev <buguev@tochka.com>
2022-11-04 15:09:27 +0500

    Чтобы линтер не ругался

--- backend/tests/resources/mocks.py
+++ backend/tests/resources/mocks.py
@@ -1,6 +1,6 @@
 def mock_vk_code_response_success():
     return {
-        'access_token': '533bacf01e11f55b536a565b57531ac114461ae8736d6506a3',
+        'access_token': 'test_token',
         'expires_in': 86399,
         'user_id': 1234567,
         'email': 'buguev.nikita@gmail.com'
