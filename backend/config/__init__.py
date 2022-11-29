5383cd9a6e943c4a07b228588eaaf4b996341f76
Nikita Buguev <buguev.nikita@gmail.com>
2022-11-29 14:14:14 +0500

    Убрал остатки дебага

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -30,17 +30,3 @@ handler.setLevel(LOG_LEVEL)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 root.addHandler(handler)
-
-
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.try:
-
-http_client.HTTPConnection.debuglevel = 1
-
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger('requests.packages.urllib3')
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
