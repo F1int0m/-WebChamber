52711bd2cbe45533aa19bdc173705a4c9ec39c43
Nikita Buguev <buguev@tochka.com>
2022-12-07 17:13:14 +0500

    Теперь живем с nginx'ом, можно в прод

--- backend/common/clients/minio_cient.py
+++ backend/common/clients/minio_cient.py
@@ -16,10 +16,11 @@ class MinioClient:
             secret_key=config.MINIO_ROOT_PASSWORD,
         )
 
-    def upload_file(self, file_data, file_name: str):
+    def upload_file(self, file_data, file_name: str, mime_type='image/jpg'):
         result = self.client.put_object(
             bucket_name=self.bucket,
             object_name=file_name,
+            content_type=mime_type,
             data=file_data,
             length=-1,
             part_size=10 * 1024 * 1024
