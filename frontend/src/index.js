c4c37b79d357be678a1ef770b12d4a1370f00a48
Nikita Buguev <buguev@tochka.com>
2023-01-22 17:04:23 +0500

    Почти починил роутинг, надо тестить в бою

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,10 +8,21 @@ import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function (error) {
+        console.log('Service worker registration failed:', error);
+    });
+} else {
+    console.log('Service workers are not supported.');
+}
+
 root.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App />
+            <App/>
         </Provider>
     </BrowserRouter>
 );
