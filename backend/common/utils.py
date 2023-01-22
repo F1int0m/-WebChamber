d15d7e5752636ec1014bb086650105d447c3eb2b
Nikita Buguev <buguev@tochka.com>
2023-01-22 14:56:03 +0500

    Поправил входной вид даты

--- backend/common/utils.py
+++ backend/common/utils.py
@@ -12,5 +12,13 @@ def now_utc():
     return datetime.datetime.now(pytz.UTC)
 
 
+def parse_datetime(datetime_string: str) -> datetime.datetime:
+    return datetime.datetime.fromisoformat(datetime_string)
+
+
+def convert_to_iso_8601_with_z_suffix(dt: datetime) -> str:
+    return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
 def create_default_nickname():
     return f'WebChamber_user_{uuid_str()}'
