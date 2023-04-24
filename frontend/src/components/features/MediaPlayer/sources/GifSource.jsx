ce63f9f9aa80235ac1ebd58a79c26a48b6e6c01e
Алеkсий <57590148+Beck-Rollyhug@users.noreply.github.com>
2023-04-24 10:24:00 +0500

    libgif живой!!!

--- frontend/src/components/features/MediaPlayer/sources/GifSource.jsx
+++ frontend/src/components/features/MediaPlayer/sources/GifSource.jsx
@@ -2,16 +2,19 @@ import React, {useEffect, useState} from 'react';
 
 import style from './Sourse.module.scss'
 import ControlPanel from "../ControlPanel/ControlPanel";
-import Loader from "../../../../view/components/Loader/Loader";
+// import Loader from "../../../../view/components/Loader/Loader";
 
-
+import SuperGif from 'libgif';
+import source from './legs.gif'
 
 // TODO: update webpack from 5 to 5.75+
 // Cross-realm object access in Webpack 5 - https://github.com/advisories/GHSA-hc6q-2mpp-qw7j
 // - appears after installing libgif
 
-const GifSource = ({source}) => {
+const GifSource = (source0) => {
     const [isLoading, setIsLoading] = useState(true)
+
+
 
     useEffect(() => {
         async function handleLoading() {
@@ -21,14 +24,35 @@ const GifSource = ({source}) => {
         handleLoading().then()
     }, [])
 
+
+
+    const img_tag = document.getElementById('img-source')
+
+    if (img_tag) {
+        console.log('found img:', img_tag)
+        console.log('type of:', typeof  img_tag)
+
+        let super_gif = new SuperGif({gif: img_tag});
+        super_gif.load_url(source); // always first after creating
+        // super_gif.pause();
+
+    }
+    else {
+        console.log('err:', img_tag)
+
+        console.log('img:', img_tag)
+        console.log('type of:', typeof  img_tag)
+    }
+
     return (
         <div className={style.mainContainer}>
             <div className={style.gifBox}>
-                {isLoading ?
-                    <Loader/>
-                    :
-                    <img src={source} alt={'gif-file'} className={style.container}/>
-                }
+                <img  alt={'gif-file'} className={style.container} id={'img-source'}/>
+                {/*{isLoading ?*/}
+                {/*    <Loader/>*/}
+                {/*    :*/}
+                {/*    <img src={source} alt={'gif-file'} className={style.container} id={'img-source'}/>*/}
+                {/*}*/}
             </div>
             <ControlPanel/>
         </div>
